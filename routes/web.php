977696dd6a836f8d3a232f785072450b83be50f0
s1mpc4rd <isabelaidrobo340@gmail.com>
2025-04-04 19:50:23 -0500

    cambio en la página del index

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 
 // Ruta para la página principal
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Rutas públicas
